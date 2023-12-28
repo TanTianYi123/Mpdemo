8fa663f9736f66ebe8f1ecaa205559383019f8c7
TanTianYi123 <1847419484@qq.com>
2023-12-28 16:28:35 +0800

    云边端项目demo

---
+++ src/main/java/com/example/mpdemo2/information/service/InformationService.java
@@ -0,0 +1,13 @@
+package com.example.mpdemo2.information.service;
+
+import com.example.mpdemo2.information.domain.Information;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Administrator
+* @description 针对表【information】的数据库操作Service
+* @createDate 2023-12-28 16:28:14
+*/
+public interface InformationService extends IService<Information> {
+
+}
